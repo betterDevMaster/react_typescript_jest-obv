e31ea627e6d19dc48e76c96d5240f2f75402de46
Mike Wu <mike@obv.io>
2020-09-05 22:59:34 +0900

    Remove SimpleBlog2 example template

--- src/Dashboard/index.tsx
+++ src/Dashboard/index.tsx
@@ -4,20 +4,13 @@ import {
   SIMPLE_BLOG,
   SimpleBlog,
 } from 'Dashboard/templates/SimpleBlog'
-import {
-  SIMPLE_BLOG_2,
-  SimpleBlog2,
-  SimpleBlog2Dashboard,
-} from 'Dashboard/templates/SimpleBlog2'
 
-export type Dashboard = SimpleBlogDashboard | SimpleBlog2Dashboard
+export type Dashboard = SimpleBlogDashboard
 
 export default function Dashboard(props: {dashboard: Dashboard}) {
   switch (props.dashboard.template) {
     case SIMPLE_BLOG:
       return <SimpleBlog dashboard={props.dashboard} />
-    case SIMPLE_BLOG_2:
-      return <SimpleBlog2 dashboard={props.dashboard} />
     default:
       // Need to handle undefined case explicitly, because the dashboard could
       // have been bound globally (via Laravel), and there's no way to
