3278fbc7bfb4cfd92c545b2fc468db1aafade885
Mike Wu <mike@obv.io>
2020-09-10 22:18:07 +0900

    Remove seed data

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import React from 'react'
 import Dashboard from 'Dashboard'
 import ThemeProvider from 'ui/theme/ThemeProvider'
-import {fakeSimpleBlog} from 'Dashboard/templates/SimpleBlog/__utils__/factory'
-import {fakeUser} from 'user/__utils__/factory'
-import {fakePoints} from 'Dashboard/components/PointsSummary/__utils__/factory'
-import {fakeNavButton} from 'Dashboard/components/NavButton/__utils__/factory'
+// import {fakeSimpleBlog} from 'Dashboard/templates/SimpleBlog/__utils__/factory'
+// import {fakeUser} from 'user/__utils__/factory'
+// import {fakePoints} from 'Dashboard/components/PointsSummary/__utils__/factory'
+// import {fakeNavButton} from 'Dashboard/components/NavButton/__utils__/factory'
 
-const OBV_DASHBOARD = fakeSimpleBlog({
-  primaryColor: '#ea202e',
-  points: fakePoints(),
-  sidebar: {
-    background: '#000000',
-    textColor: '#Ffffff',
-    navButtons: Array.from({length: 5}, fakeNavButton),
-  },
-})
+// const OBV_DASHBOARD = fakeSimpleBlog({
+//   primaryColor: '#ea202e',
+//   points: fakePoints(),
+//   sidebar: {
+//     background: '#000000',
+//     textColor: '#Ffffff',
+//     navButtons: Array.from({length: 5}, fakeNavButton),
+//   },
+// })
 
-const OBV_USER = fakeUser()
+// const OBV_USER = fakeUser()
 
 function App() {
   return (
