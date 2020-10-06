33ea39273f10968bf0e9c24bfbb5691452c4ad74
Mike Wu <mike@obv.io>
2020-10-06 22:00:52 +0900

    Move main nav button list tests

--- src/Dashboard/Template/SimpleBlog/MainNavButtonList/MainNavButton/__tests__/MainNavButton.test.tsx
+++ src/Dashboard/Template/SimpleBlog/MainNavButtonList/MainNavButton/__tests__/MainNavButton.test.tsx
@@ -9,29 +9,6 @@ import {createEntityList} from 'lib/list'
 import {clickEdit} from '__utils__/edit'
 import {fireEvent} from '@testing-library/react'
 
-it('should render main nav buttons', async () => {
-  const buttons = Array.from(
-    {
-      length: faker.random.number({min: 1, max: 4}),
-    },
-    fakeNavButtonWithSize,
-  )
-
-  const mainNavButtons = createEntityList(buttons)
-  const {findAllByLabelText} = render(
-    <Dashboard
-      isEditMode={false}
-      dashboard={fakeSimpleBlog({
-        mainNavButtons,
-      })}
-      user={fakeUser()}
-    />,
-  )
-
-  const buttonsEls = await findAllByLabelText('main nav button')
-  expect(buttonsEls.length).toBe(mainNavButtons.ids.length)
-})
-
 it('should edit the selected button', async () => {
   const buttons = Array.from(
     {
@@ -74,35 +51,4 @@ it('should edit the selected button', async () => {
 
   const updatedEl = await findByText(updatedValue)
   expect(updatedEl).toBeInTheDocument()
-})
-
-it('should add a new main nav button', async () => {
-  const numButtons = faker.random.number({min: 1, max: 4})
-
-  const buttons = Array.from(
-    {
-      length: numButtons,
-    },
-    fakeNavButtonWithSize,
-  )
-
-  const mainNavButtons = createEntityList(buttons)
-
-  const {findAllByLabelText, findByLabelText} = render(
-    <Dashboard
-      isEditMode={true}
-      dashboard={fakeSimpleBlog({
-        mainNavButtons,
-      })}
-      user={fakeUser()}
-    />,
-  )
-
-  const buttonEls = () => findAllByLabelText('main nav button')
-
-  expect((await buttonEls()).length).toBe(numButtons)
-
-  fireEvent.click(await findByLabelText('add main nav button'))
-
-  expect((await buttonEls()).length).toBe(numButtons + 1)
 })
