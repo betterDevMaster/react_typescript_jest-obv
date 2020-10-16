ef5ccd42917334d46c0c96ea3a047bff1fe2a9d5
Mike Wu <mike@obv.io>
2020-10-16 13:55:22 +0900

    Add the rest of sidebar nav butotn tests

--- src/Dashboard/Template/SimpleBlog/Sidebar/SidebarNav/__tests__/SidebarNavButtonList.test.tsx
+++ src/Dashboard/Template/SimpleBlog/Sidebar/SidebarNav/__tests__/SidebarNavButtonList.test.tsx
@@ -4,12 +4,10 @@ import {fakeSimpleBlog} from 'Dashboard/Template/SimpleBlog/__utils__/factory'
 import {fakeUser} from 'user/__utils__/factory'
 import Dashboard from 'Dashboard'
 import {render} from '__utils__/render'
-import {
-  fakeNavButton,
-  fakeNavButtonWithSize,
-} from 'Dashboard/components/NavButton/__utils__/factory'
+import {fakeNavButton} from 'Dashboard/components/NavButton/__utils__/factory'
 import {createEntityList} from 'lib/list'
 import {fireEvent} from '@testing-library/react'
+import {clickEdit} from '__utils__/edit'
 
 it('should render sidebarNavButtons', () => {
   const dashboard = fakeSimpleBlog({
@@ -70,4 +68,78 @@ it('should add a new sidebar nav button', async () => {
   fireEvent.click(await findByLabelText('add sidebar nav button'))
 
   expect((await buttonEls()).length).toBe(numButtons + 1)
+})
+
+it('should edit the selected button', async () => {
+  const buttons = Array.from(
+    {
+      length: faker.random.number({min: 1, max: 4}),
+    },
+    fakeNavButton,
+  )
+
+  const sidebarNavButtons = createEntityList(buttons)
+  const {findByLabelText, findByText} = render(
+    <Dashboard
+      isEditMode={true}
+      dashboard={fakeSimpleBlog({
+        sidebarNavButtons,
+      })}
+      user={fakeUser()}
+    />,
+  )
+
+  const button = faker.random.arrayElement(buttons)
+  const buttonEl = await findByText(button.text)
+
+  clickEdit(buttonEl)
+
+  const textInput = (await findByLabelText(
+    'button name input',
+  )) as HTMLInputElement
+  expect(textInput.value).toBe(button.text)
+
+  const updatedValue = faker.random.word()
+
+  fireEvent.change(textInput, {
+    target: {
+      value: updatedValue,
+    },
+  })
+
+  fireEvent.click(await findByLabelText('close config dialog'))
+
+  const updatedEl = await findByText(updatedValue)
+  expect(updatedEl).toBeInTheDocument()
+})
+
+it('should remove the button', async () => {
+  const numButtons = faker.random.number({min: 2, max: 4})
+
+  const buttons = Array.from({length: numButtons}, fakeNavButton)
+  const sidebarNavButtons = createEntityList(buttons)
+
+  const {findAllByLabelText, findByLabelText, queryByText} = render(
+    <Dashboard
+      isEditMode={true}
+      dashboard={fakeSimpleBlog({
+        sidebarNavButtons,
+      })}
+      user={fakeUser()}
+    />,
+  )
+
+  const buttonEls = () => findAllByLabelText('sidebar nav button')
+  expect((await buttonEls()).length).toBe(numButtons)
+
+  const target = faker.random.arrayElement(await buttonEls())
+  expect(queryByText(target.textContent!)).toBeInTheDocument()
+
+  clickEdit(target)
+
+  fireEvent.click(await findByLabelText('remove button'))
+
+  expect((await buttonEls()).length).toBe(numButtons - 1)
+
+  expect(queryByText(target.textContent!)).not.toBeInTheDocument()
 })
