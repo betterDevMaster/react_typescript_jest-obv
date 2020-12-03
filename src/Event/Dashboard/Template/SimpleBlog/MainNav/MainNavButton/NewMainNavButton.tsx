e52087bcdcbdfa3bcfe3dde14916b5a32da852ec
Mike Wu <mike@obv.io>
2020-12-03 16:51:45 +0900

    Show config after adding button

--- src/Event/Dashboard/Template/SimpleBlog/MainNav/MainNavButton/NewMainNavButton.tsx
+++ src/Event/Dashboard/Template/SimpleBlog/MainNav/MainNavButton/NewMainNavButton.tsx
@@ -1,16 +1,20 @@
 import Button from '@material-ui/core/Button'
 import Grid from '@material-ui/core/Grid'
 import {NavButtonWithSize} from 'Event/Dashboard/components/NavButton'
+import {setConfig} from 'Event/Dashboard/editor/state/actions'
 import {
   useDashboard,
   useUpdateDashboard,
 } from 'Event/Dashboard/state/DashboardProvider'
+import {MAIN_NAV_BUTTON} from 'Event/Dashboard/Template/SimpleBlog/MainNav/MainNavButton'
 import React from 'react'
+import {useDispatch} from 'react-redux'
 import {v4 as uid} from 'uuid'
 
 export default function NewMainNavButton() {
   const {mainNav: buttons} = useDashboard()
   const updateDashboard = useUpdateDashboard()
+  const dispatch = useDispatch()
 
   if (!buttons) {
     return null
@@ -36,6 +40,9 @@ export default function NewMainNavButton() {
         ids,
       },
     })
+
+    // Show config after adding button
+    dispatch(setConfig({type: MAIN_NAV_BUTTON, id}))
   }
   return (
     <Grid item xs={12}>
