02c9e6d9e4ddba076f4458c1688c52637c93564d
Mike Wu <mike@obv.io>
2020-12-24 00:47:24 +0900

    Add test to make sure file path was saved

--- src/Event/Dashboard/components/ResourceList/ResourceUpload/__tests__/ResourceUpload.test.tsx
+++ src/Event/Dashboard/components/ResourceList/ResourceUpload/__tests__/ResourceUpload.test.tsx
@@ -9,8 +9,10 @@ import React from 'react'
 import {clickEdit} from '__utils__/edit'
 import {render} from '__utils__/render'
 import axios from 'axios'
+import {mockRxJsAjax} from 'store/__utils__/MockStoreProvider'
 
-const mockPost = axios.post as jest.Mock
+const mockAjaxPost = axios.post as jest.Mock
+const mockRxPost = mockRxJsAjax.post as jest.Mock
 
 it('should upload a file', async () => {
   const template = fakeSimpleBlog({
@@ -43,7 +45,7 @@ it('should upload a file', async () => {
   const filePath = 'somegeneratedfilepath.pdf'
 
   // Server responds with file path
-  mockPost.mockImplementationOnce(() =>
+  mockAjaxPost.mockImplementationOnce(() =>
     Promise.resolve({
       data: {
         file: filePath,
@@ -57,13 +59,26 @@ it('should upload a file', async () => {
     },
   })
 
+  // Uploaded?
   await wait(() => {
-    expect(mockPost).toHaveBeenCalledTimes(1)
+    expect(mockAjaxPost).toHaveBeenCalledTimes(1)
   })
 
-  const [url] = mockPost.mock.calls[0]
+  const [url] = mockAjaxPost.mock.calls[0]
   expect(url).toMatch(`/events/${event.slug}/resources`) // was posted to resources
 
-  // Has existing file
+  // Shows remove button, ie. has existing file now
   expect(await findByLabelText('remove resource file')).toBeInTheDocument()
+
+  // saved?
+  await wait(() => {
+    expect(mockRxPost).toHaveBeenCalledTimes(1)
+  })
+
+  const [saveUrl, savedData] = mockRxPost.mock.calls[0]
+  expect(saveUrl).toMatch(`/events/${event.slug}`)
+  // Saved returned file path
+  expect(savedData.template.resourceList.resources[0]['filePath']).toBe(
+    filePath,
+  )
 })
