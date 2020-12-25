4a6c6ed25819f4cf11f8c897a834c04f226d9c50
Mike Wu <mike@obv.io>
2020-12-25 13:10:29 +0900

    Update sending emoji without redux

--- src/Event/Dashboard/state/epics/index.ts
+++ src/Event/Dashboard/state/epics/index.ts
@@ -1,10 +1,8 @@
 import {createDashboardEpic} from 'Event/Dashboard/state/epics/create-dashboard'
 import {saveDashboardEpic} from 'Event/Dashboard/state/epics/save-dashboard'
-import {sendEmojiEpic} from 'Event/Dashboard/state/epics/send-emoji'
 import {combineEpics} from 'redux-observable'
 
 export const dashboardEpics = combineEpics(
   saveDashboardEpic,
   createDashboardEpic,
-  sendEmojiEpic,
 )
