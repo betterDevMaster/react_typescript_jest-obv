f71a194aa56e9f5a0c3ce5ea101bf0fc85d64510
Dzianis <dzianis@obv.io>
2020-12-29 06:51:49 +0300

    update emoji

--- src/organization/Events/EventConfig/Emoji.tsx
+++ src/organization/Events/EventConfig/Emoji.tsx
@@ -1,28 +1,9 @@
-import Button from '@material-ui/core/Button'
 import styled from 'styled-components'
-import InputLabel from '@material-ui/core/InputLabel'
-import withStyles from '@material-ui/core/styles/withStyles'
-import TextField from '@material-ui/core/TextField'
-import Typography from '@material-ui/core/Typography'
-import {spacing} from 'lib/ui/theme'
-import Page from 'organization/user/Layout/Page'
 import React, {useCallback, useEffect, useRef, useState} from 'react'
-import FormHelperText from '@material-ui/core/FormHelperText'
 import {useEvent} from 'Event/EventProvider'
 import {api} from 'lib/url'
-import {useOrganization} from 'organization/OrganizationProvider'
-import {ObvioEvent} from 'Event'
-import {setEvent} from 'Event/state/actions'
-import {useDispatch} from 'react-redux'
-import {useEventRoutes} from 'Event/url'
-import {useHistory} from 'react-router-dom'
-import {waiverLogoPath} from 'Event/Step2/Waiver'
-import {fetchFile} from 'lib/http-client'
-
-import {emojiWithName} from 'Event/Dashboard/components/EmojiList/emoji'
 import {eventClient} from 'Event/api-client'
 import EmojiRender from './EmojiRender'
-import {random} from 'faker'
 
 export interface floatingEmojiData {
   id: number
@@ -38,38 +19,11 @@ export interface EmojiStateType {
   isRendering: boolean
 }
 
-// const mockup = [
-//   {
-//     content: 'heart',
-//     number: 2,
-//     duration: 5,
-//     repeat: 2,
-//     size: 1,
-//   },
-//   {
-//     content: 'clap',
-//     number: 2,
-//     duration: 2,
-//     repeat: 2,
-//     size: 1.5,
-//   },
-//   {
-//     content: 'laugh',
-//     number: 2,
-//     duration: 3,
-//     repeat: 2,
-//     size: 1.2,
-//   },
-// ]
-
 const injectStyle = (style: string) => {
   const styleElement = document.createElement('style') as HTMLStyleElement
   let styleSheet = null
-
   document.head.appendChild(styleElement)
-
   styleSheet = styleElement.sheet as CSSStyleSheet
-
   styleSheet.insertRule(style, styleSheet.cssRules.length)
 }
 
@@ -77,52 +31,25 @@ export default function Emoji() {
   const {event} = useEvent()
   const [emojiList, setEmojiList] = useState<EmojiStateType[]>([])
   const [timer, setTimer] = useState<Number>()
-
   useEffect(() => {
-    /* initialize emoji styles */
-    const defaultEmojiStyle = `
-      default-emoji {
-        marging-top: 110%
-      }`
     const animateBubble = `
       @-webkit-keyframes animateBubble {
-        0% { margin-top: 110% }
-        100% { margin-top: -10% }
+        0% { top: calc(100% + 50px) }
+        100% {top: -50px }
       }`
     const sideWays = `
         @-webkit-keyframes sideWays { 
           0% { margin-left:-50px }
           100% { margin-left:50px }
       }`
-
-    injectStyle(defaultEmojiStyle)
     injectStyle(animateBubble)
     injectStyle(sideWays)
-
     /* fetch emoji updates */
     const interval = setInterval(() => {
-      /* TODO: removed afterwards */
-      // let randomNumber = parseInt((Math.random() * 20).toString())
-      // const isAdd = randomNumber > 3
-      // if (isAdd) {
-      //   const newEmoji: EmojiStateType = {
-      //     data: JSON.parse(
-      //       JSON.stringify({
-      //         ...mockup[randomNumber % 3],
-      //         id: Date.now(),
-      //       }),
-      //     ),
-      //     isRendering: false,
-      //   }
-      //   emojiList.push(newEmoji)
-      //   setEmojiList(JSON.parse(JSON.stringify(emojiList)))
-      // }
-
       const url = api(`/events/${event.slug}/getEmoji`)
       eventClient
         .get(url)
         .then((res) => {
-          // res will josn data ex {"data":["thumb_up","thumb_up"]}
           JSON.parse(JSON.stringify(res)).data.forEach((image: string) => {
             const newEmoji: EmojiStateType = {
               data: {
@@ -138,8 +65,6 @@ export default function Emoji() {
             emojiList.push(newEmoji)
           })
           setEmojiList(JSON.parse(JSON.stringify(emojiList)))
-
-          // console.log(emojiWithName('heart').image)
         })
         .catch((error) => {
           // ignore errors, prevent failing to send emoji from crashing app
@@ -152,7 +77,6 @@ export default function Emoji() {
   }, [])
 
   const emojiRenderFinished = useCallback((emojiInfo) => {
-    // console.log('delete = ', emojiInfo.data.id)
     const indexOfEmoji = emojiList.findIndex(
       (item) => item.data.id === emojiInfo.data.id,
     )
@@ -188,12 +112,14 @@ export default function Emoji() {
 }
 
 const MainBody = styled.div`
-  height: calc(100vh - 64px);
+  height: 100vh;
   width: 100%;
   background: black;
-
+  position: absolute;
+  margin-top: -64px;
+  overflow: hidden;
   img {
-    width: 68px;
-    height: 68px;
+    width: 66px;
+    height: 66px;
   }
 `
--- src/organization/Events/EventConfig/EmojiRender.tsx
+++ src/organization/Events/EventConfig/EmojiRender.tsx
@@ -1,12 +1,10 @@
 import styled from 'styled-components'
 import React, {memo, useEffect, useMemo, useRef, useState} from 'react'
-
 import {emojiWithName} from 'Event/Dashboard/components/EmojiList/emoji'
 import {EmojiStateType} from './Emoji'
 
 const EMOJI_WIDTH_BIND: number = 50
 const EMOJI_DEFAULT_SIZE: number = 68
-
 interface EmojiRenderPropsType {
   emojiInfo: EmojiStateType
   finished: Function
@@ -17,31 +15,16 @@ function EmojiRender(props: EmojiRenderPropsType) {
   const {emojiInfo, finished} = props
   const [repeat, setRepeat] = useState(1)
   const [keyframeName, setKeyframeName] = useState('')
-
   useEffect(() => {
     const timer = setTimeout(() => {
       finished(emojiInfo)
     }, emojiInfo.data.duration * 1000)
   }, [])
-
   useEffect(() => {
-    const intervalPercent = 100 / (emojiInfo.data.duration * 2)
-    const moveHeightPerSec = 100 / (emojiInfo.data.duration * 2)
-    const frames = Array(emojiInfo.data.duration * 2)
-      .fill(1)
-      .map((_value, index) => {
-        return `
-        ${(index + 0.5) * intervalPercent}% { transform: translateX(${
-          EMOJI_WIDTH_BIND * (index % 2 ? -1 : 1)
-        }px);}
-      `
-      })
-
     setKeyframeName(
       `animateBubble ${emojiInfo.data.duration}s linear, sideWays 1s ease-in-out infinite alternate`,
     )
   }, [])
-
   return (
     <EmojiRenderContainer
       style={{
@@ -50,7 +33,6 @@ function EmojiRender(props: EmojiRenderPropsType) {
         width: emojiInfo.data.size * EMOJI_DEFAULT_SIZE,
         height: emojiInfo.data.size * EMOJI_DEFAULT_SIZE,
       }}
-      className="default-emoji"
     >
       <img
         aria-label="event emoji"
