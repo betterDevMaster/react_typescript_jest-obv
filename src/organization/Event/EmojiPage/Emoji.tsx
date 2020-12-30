808a48e87db2a063466038ecf7b56f2dcca1b398
Mike Wu <mike@obv.io>
2020-12-30 21:13:52 +0900

    Clean up Emoji

--- src/organization/Event/EmojiPage/Emoji.tsx
+++ src/organization/Event/EmojiPage/Emoji.tsx
@@ -1,62 +1,88 @@
 import styled from 'styled-components'
-import React, {useEffect, useState} from 'react'
+import React, {useEffect} from 'react'
 import {emojiWithName} from 'Event/Dashboard/components/EmojiList/emoji'
 import {v4 as uuid} from 'uuid'
-import {EmojiStateType} from 'organization/Event/EmojiPage'
 
-const EMOJI_DEFAULT_SIZE: number = 68
+const DEFAULT_SIZE: number = 68
+
+export interface EmojiStateType {
+  id: string
+  content: string
+  number: number
+  duration: number
+  repeat: number
+  size: number
+}
+
 interface EmojiProps {
   emoji: EmojiStateType
   onComplete: (emoji: EmojiStateType) => void
 }
 
 export default React.memo<EmojiProps>(Emoji, (_, nextProps) => {
-  return Boolean(nextProps.emoji.data.id)
+  return Boolean(nextProps.emoji.id)
 })
 
 function Emoji(props: EmojiProps) {
   const {emoji, onComplete: finished} = props
-  const [keyframeName, setKeyframeName] = useState('')
+
   useEffect(() => {
     setTimeout(() => {
       finished(emoji)
-    }, emoji.data.duration * 1000)
+    }, emoji.duration * 1000)
   }, [])
-  useEffect(() => {
-    setKeyframeName(
-      `animateBubble ${emoji.data.duration}s linear, sideWays 1s ease-in-out infinite alternate`,
-    )
-  }, [])
+
+  const position = Math.random() * 80 + 10
+  const size = emoji.size * DEFAULT_SIZE
+
   return (
-    <Box
-      style={{
-        WebkitAnimation: `${keyframeName}`,
-        left: `${Math.random() * 80 + 10}%`,
-        width: emoji.data.size * EMOJI_DEFAULT_SIZE,
-        height: emoji.data.size * EMOJI_DEFAULT_SIZE,
-      }}
-    >
+    <Box duration={emoji.duration} position={position} size={size}>
       <img
-        aria-label="event emoji"
-        src={emojiWithName(emoji.data.content).image}
-        alt={emoji.data.content}
+        aria-label="emoji"
+        src={emojiWithName(emoji.content).image}
+        alt={emoji.content}
       />
     </Box>
   )
 }
 
 export const createEmoji = (image: string): EmojiStateType => ({
-  data: {
-    id: uuid(),
-    content: image,
-    number: 2,
-    duration: Math.random() * 10 + 1,
-    repeat: 1,
-    size: Math.random() + 1,
-  },
-  isRendering: false,
+  id: uuid(),
+  content: image,
+  number: 2,
+  duration: Math.random() * 10 + 1,
+  repeat: 1,
+  size: Math.random() + 1,
 })
 
-const Box = styled.div`
+const Box = styled.div<{duration: number; position: number; size: number}>`
   position: absolute;
+  animation: animateBubble ${(props) => props.duration}s linear,
+    sideWays 1s ease-in-out infinite alternate;
+  left: ${(props) => props.position}%;
+  width: ${(props) => props.position}px;
+  height: ${(props) => props.position}px;
+
+  img {
+    width: 66px;
+    height: 66px;
+  }
+
+  @keyframes animateBubble {
+    0% {
+      top: calc(100% + 50px);
+    }
+    100% {
+      top: -50px;
+    }
+  }
+
+  @keyframes sideWays {
+    0% {
+      margin-left: -50px;
+    }
+    100% {
+      margin-left: 50px;
+    }
+  }
 `
--- src/organization/Event/EmojiPage/index.tsx
+++ src/organization/Event/EmojiPage/index.tsx
@@ -2,32 +2,13 @@ import styled from 'styled-components'
 import React, {useCallback, useEffect, useState} from 'react'
 import {useEvent} from 'Event/EventProvider'
 import {api} from 'lib/url'
-import Emoji, {createEmoji} from './Emoji'
 import {useOrganization} from 'organization/OrganizationProvider'
-
-export interface floatingEmojiData {
-  id: string
-  content: string
-  number: number
-  duration: number
-  repeat: number
-  size: number
-}
-
-export interface EmojiStateType {
-  data: floatingEmojiData
-  isRendering: boolean
-}
+import Emoji, {
+  createEmoji,
+  EmojiStateType,
+} from 'organization/Event/EmojiPage/Emoji'
 
 export type Emojis = string[]
-
-const injectStyle = (style: string) => {
-  const styleElement = document.createElement('style') as HTMLStyleElement
-  let styleSheet = null
-  document.head.appendChild(styleElement)
-  styleSheet = styleElement.sheet as CSSStyleSheet
-  styleSheet.insertRule(style, styleSheet.cssRules.length)
-}
 
 const POLL_INTERVAL_MS = 1000
 
@@ -36,19 +17,6 @@ export default function EmojiPage() {
   const fetchEmojis = useFetchEmojis()
 
   useEffect(() => {
-    const animateBubble = `
-      @-webkit-keyframes animateBubble {
-        0% { top: calc(100% + 50px) }
-        100% {top: -50px }
-      }`
-    const sideWays = `
-        @-webkit-keyframes sideWays { 
-          0% { margin-left:-50px }
-          100% { margin-left:50px }
-      }`
-    injectStyle(animateBubble)
-    injectStyle(sideWays)
-    /* fetch emoji updates */
     const interval = setInterval(() => {
       fetchEmojis()
         .then((emojis) => {
@@ -69,23 +37,17 @@ export default function EmojiPage() {
   }, [])
 
   const removeEmoji = useCallback((emojiInfo) => {
-    const indexOfEmoji = emojiList.findIndex(
-      (item) => item.data.id === emojiInfo.data.id,
-    )
+    const indexOfEmoji = emojiList.findIndex((item) => item.id === emojiInfo.id)
     emojiList.splice(indexOfEmoji, 1)
     setEmojiList(JSON.parse(JSON.stringify(emojiList)))
   }, [])
 
   return (
-    <MainBody>
-      <div className="float-container" id="float-container">
-        {emojiList.map((emoji) => {
-          return (
-            <Emoji emoji={emoji} key={emoji.data.id} onComplete={removeEmoji} />
-          )
-        })}
-      </div>
-    </MainBody>
+    <Container>
+      {emojiList.map((emoji) => {
+        return <Emoji emoji={emoji} key={emoji.id} onComplete={removeEmoji} />
+      })}
+    </Container>
   )
 }
 
@@ -97,14 +59,10 @@ function useFetchEmojis() {
   return () => client.get<Emojis>(url)
 }
 
-const MainBody = styled.div`
+const Container = styled.div`
   height: 100vh;
   width: 100%;
   background: black;
   position: absolute;
   overflow: hidden;
-  img {
-    width: 66px;
-    height: 66px;
-  }
 `
