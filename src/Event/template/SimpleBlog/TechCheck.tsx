d8d39048976cf1b70320257a775e902709e348d0
Mike Wu <mike@obv.io>
2021-02-03 23:51:59 +0900

    Remove tech check border

--- src/Event/template/SimpleBlog/TechCheck.tsx
+++ src/Event/template/SimpleBlog/TechCheck.tsx
@@ -4,7 +4,6 @@ import Container from '@material-ui/core/Container'
 import {User} from 'auth/user'
 import {TechCheckProps} from 'Event/Step3/TechCheck'
 import styled from 'styled-components'
-import grey from '@material-ui/core/colors/grey'
 import {Button} from '@material-ui/core'
 import {api} from 'lib/url'
 import {useEvent} from 'Event/EventProvider'
@@ -78,8 +77,6 @@ function Error(props: {children: ResponseError | null}) {
 const Body = styled.div`
   max-height: 240px;
   overflow-y: auto;
-  border: 1px solid ${grey[300]};
-  padding: 0 ${(props) => props.theme.spacing[4]};
   margin-bottom: ${(props) => props.theme.spacing[4]};
 `
 
