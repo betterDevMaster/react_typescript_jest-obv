0bf7fe6a0d2c0a1a860870a7fae3afe1529984a9
Mike Wu <hello@mikewu.dev>
2021-02-22 23:19:32 +0900

    Add back registration

--- src/obvio/auth/__tests__/Registration.test.tsx
+++ src/obvio/auth/__tests__/Registration.test.tsx
@@ -11,53 +11,51 @@ const mockPost = mockAxios.post as jest.Mock
 const mockGet = mockAxios.get as jest.Mock
 
 it('should register, and sign in', async () => {
-  expect(true).toBe(true)
+  const token = 'thesecrettoken'
+  mockPost.mockImplementationOnce(() =>
+    Promise.resolve({data: {access_token: token}}),
+  )
+  mockGet.mockImplementationOnce(() => Promise.resolve({data: fakeUser()}))
 
-  // const token = 'thesecrettoken'
-  // mockPost.mockImplementationOnce(() =>
-  //   Promise.resolve({data: {access_token: token}}),
-  // )
-  // mockGet.mockImplementationOnce(() => Promise.resolve({data: fakeUser()}))
+  const {findByLabelText, findByText} = render(<App />)
 
-  // const {findByLabelText, findByText} = render(<App />)
+  user.click(await findByLabelText('create account'))
 
-  // user.click(await findByLabelText('create account'))
+  const email = faker.internet.email()
+  const password = 'mypw'
+  const firstName = faker.name.firstName()
+  const lastName = faker.name.lastName()
 
-  // const email = faker.internet.email()
-  // const password = 'mypw'
-  // const firstName = faker.name.firstName()
-  // const lastName = faker.name.lastName()
+  user.type(await findByLabelText('first name'), firstName)
+  user.type(await findByLabelText('last name'), lastName)
+  user.type(await findByLabelText('email'), email)
+  user.type(await findByLabelText('password'), password)
+  user.type(await findByLabelText('password confirmation'), password)
 
-  // user.type(await findByLabelText('first name'), firstName)
-  // user.type(await findByLabelText('last name'), lastName)
-  // user.type(await findByLabelText('email'), email)
-  // user.type(await findByLabelText('password'), password)
-  // user.type(await findByLabelText('password confirmation'), password)
+  user.click(await findByLabelText('register'))
 
-  // user.click(await findByLabelText('register'))
+  await wait(() => {
+    expect(mockPost).toHaveBeenCalledTimes(1)
+  })
 
-  // await wait(() => {
-  //   expect(mockPost).toHaveBeenCalledTimes(1)
-  // })
+  // Submitted correct data
+  const data = mockPost.mock.calls[0][1]
+  expect(data.email).toBe(email)
+  expect(data.first_name).toBe(firstName)
+  expect(data.last_name).toBe(lastName)
+  expect(data.password).toBe(password)
+  expect(data.password_confirmation).toBe(password)
 
-  // // Submitted correct data
-  // const data = mockPost.mock.calls[0][1]
-  // expect(data.email).toBe(email)
-  // expect(data.first_name).toBe(firstName)
-  // expect(data.last_name).toBe(lastName)
-  // expect(data.password).toBe(password)
-  // expect(data.password_confirmation).toBe(password)
+  await wait(() => {
+    expect(mockGet).toHaveBeenCalledTimes(2)
+  })
 
-  // await wait(() => {
-  //   expect(mockGet).toHaveBeenCalledTimes(2)
-  // })
+  // token saved
+  expect(window.localStorage.getItem('__obvio_user_token__')).toBe(token)
 
-  // // token saved
-  // expect(window.localStorage.getItem('__obvio_user_token__')).toBe(token)
+  // Requested user?
+  const authHeader = mockGet.mock.calls[0][1]['headers']['Authorization']
+  expect(authHeader).toBe(`Bearer ${token}`)
 
-  // // Requested user?
-  // const authHeader = mockGet.mock.calls[0][1]['headers']['Authorization']
-  // expect(authHeader).toBe(`Bearer ${token}`)
-
-  // expect(await findByText('Logout')).toBeInTheDocument()
+  expect(await findByText('Logout')).toBeInTheDocument()
 })
