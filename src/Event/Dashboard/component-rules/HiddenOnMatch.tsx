38f5f469c69b719102bfa71b064387e64e8ca5e4
Mike Wu <hello@mikewu.dev>
2021-04-10 11:32:19 +0900

    Hofix: missing rule

--- src/Event/Dashboard/component-rules/HiddenOnMatch.tsx
+++ src/Event/Dashboard/component-rules/HiddenOnMatch.tsx
@@ -4,13 +4,13 @@ import {useAttendeeProfile} from 'Event/Dashboard/component-rules/AttendeeProfil
 import {useEditMode} from 'Event/Dashboard/editor/state/edit-mode'
 
 export default function HiddenOnMatch(props: {
-  rules: Rule[]
+  rules?: Rule[]
   children: React.ReactElement
 }) {
   const {groups, tags} = useAttendeeProfile()
   const isEditMode = useEditMode()
 
-  if (isEditMode) {
+  if (isEditMode || !props.rules) {
     // Always show in edit mode or it would
     // be pretty difficult to configure a hidden component.
     return props.children
