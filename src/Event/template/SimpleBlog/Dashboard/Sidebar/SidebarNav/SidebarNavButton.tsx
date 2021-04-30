8f043aba01505877ab15fb49c72a6e2fc2a29039
Mike Wu <hello@mikewu.dev>
2021-04-30 23:12:02 +0900

    Add sidebar re-order

--- src/Event/template/SimpleBlog/Dashboard/Sidebar/SidebarNav/SidebarNavButton.tsx
+++ src/Event/template/SimpleBlog/Dashboard/Sidebar/SidebarNav/SidebarNavButton.tsx
@@ -4,14 +4,14 @@ import NavButton from 'Event/Dashboard/components/NavButton'
 import {SIDEBAR_NAV_BUTTON} from 'Event/template/SimpleBlog/Dashboard/Sidebar/SidebarNav'
 import EditComponent from 'Event/Dashboard/editor/views/EditComponent'
 import Published from 'Event/Dashboard/editor/views/Published'
+import {Draggable} from 'react-beautiful-dnd'
+import {DraggableOverlay, DragHandle} from 'lib/ui/drag-and-drop'
+import {useEditMode} from 'Event/Dashboard/editor/state/edit-mode'
 
-export default React.memo((props: NavButton & {id: string}) => (
-  <EditComponent
-    component={{
-      type: SIDEBAR_NAV_BUTTON,
-      id: props.id,
-    }}
-  >
+export default React.memo((props: NavButton & {id: string; index: number}) => {
+  const isEditMode = useEditMode()
+
+  const button = (
     <Published component={props}>
       <StyledNavButtonComponent
         aria-label="sidebar nav button"
@@ -21,8 +21,34 @@ export default React.memo((props: NavButton & {id: string}) => (
         {...props}
       />
     </Published>
-  </EditComponent>
-))
+  )
+
+  if (!isEditMode) {
+    return button
+  }
+
+  return (
+    <Draggable draggableId={props.id} index={props.index}>
+      {(provided) => (
+        <div ref={provided.innerRef} {...provided.draggableProps}>
+          <DraggableOverlay>
+            <EditComponent
+              component={{
+                type: SIDEBAR_NAV_BUTTON,
+                id: props.id,
+              }}
+            >
+              <>
+                <DragHandle handleProps={provided.dragHandleProps} />
+                {button}
+              </>
+            </EditComponent>
+          </DraggableOverlay>
+        </div>
+      )}
+    </Draggable>
+  )
+})
 
 const StyledNavButtonComponent = styled(NavButton)`
   font-size: 14px;
--- src/Event/template/SimpleBlog/Dashboard/Sidebar/SidebarNav/index.tsx
+++ src/Event/template/SimpleBlog/Dashboard/Sidebar/SidebarNav/index.tsx
@@ -3,14 +3,20 @@ import styled from 'styled-components'
 import SidebarNavButton from 'Event/template/SimpleBlog/Dashboard/Sidebar/SidebarNav/SidebarNavButton'
 import NewSidebarNavButton from 'Event/template/SimpleBlog/Dashboard/Sidebar/NewSidebarNavButton'
 import EditModeOnly from 'Event/Dashboard/editor/views/EditModeOnly'
-import {useTemplate} from 'Event/TemplateProvider'
+import {useTemplate, useUpdateTemplate} from 'Event/TemplateProvider'
 import Section from 'Event/template/SimpleBlog/Dashboard/Sidebar/Section'
 import HiddenOnMatch from 'Event/Dashboard/component-rules/HiddenOnMatch'
+import {DragDropContext, Droppable, DropResult} from 'react-beautiful-dnd'
+import {useEditMode} from 'Event/Dashboard/editor/state/edit-mode'
 export const SIDEBAR_NAV_BUTTON = 'Sidebar Nav Button'
 
 export default function SidebarNav() {
-  const {sidebarNav: buttons} = useTemplate()
-  const hasButtons = buttons.ids.length > 0
+  const {sidebarNav} = useTemplate()
+  const {ids, entities} = sidebarNav
+  const handleDrag = useHandleDrag()
+  const isEditMode = useEditMode()
+
+  const hasButtons = ids.length > 0
   if (!hasButtons) {
     return (
       <EditModeOnly>
@@ -19,30 +25,70 @@ export default function SidebarNav() {
     )
   }
 
+  const buttons = ids.map((id, index) => {
+    const button = entities[id]
+    return (
+      <HiddenOnMatch rules={button.rules} key={id}>
+        <SidebarNavButton
+          {...button}
+          id={id}
+          backgroundColor={button.backgroundColor}
+          hoverBackgroundColor={button.backgroundColor}
+          textColor={button.textColor}
+          borderWidth={button.borderWidth}
+          borderRadius={button.borderRadius}
+          borderColor={button.borderColor}
+          index={index}
+        />
+      </HiddenOnMatch>
+    )
+  })
+
+  if (!isEditMode) {
+    return <Section>{buttons}</Section>
+  }
+
   return (
     <Section>
-      {buttons.ids.map((id) => {
-        const button = buttons.entities[id]
-        return (
-          <HiddenOnMatch rules={button.rules} key={id}>
-            <SidebarNavButton
-              {...button}
-              id={id}
-              backgroundColor={button.backgroundColor}
-              hoverBackgroundColor={button.backgroundColor}
-              textColor={button.textColor}
-              borderWidth={button.borderWidth}
-              borderRadius={button.borderRadius}
-              borderColor={button.borderColor}
-            />
-          </HiddenOnMatch>
-        )
-      })}
-      <EditModeOnly>
+      <DragDropContext onDragEnd={handleDrag}>
+        <Droppable droppableId="sidebar_nav_buttons">
+          {(provided) => (
+            <div ref={provided.innerRef} {...provided.droppableProps}>
+              {buttons}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
         <StyledNewSidebarNavButton />
-      </EditModeOnly>
+      </DragDropContext>
     </Section>
   )
+}
+
+function useHandleDrag() {
+  const {sidebarNav: buttons} = useTemplate()
+  const updateTemplate = useUpdateTemplate()
+
+  return (result: DropResult) => {
+    const {destination, source} = result
+
+    if (!destination) {
+      return
+    }
+
+    const moved = Array.from(buttons.ids)
+    const [removed] = moved.splice(source.index, 1)
+    moved.splice(destination.index, 0, removed)
+
+    updateTemplate({
+      sidebarNav: {
+        ids: moved,
+        entities: {
+          ...buttons.entities,
+        },
+      },
+    })
+  }
 }
 
 const StyledNewSidebarNavButton = styled(NewSidebarNavButton)`
