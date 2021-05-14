b5f41afdd53c4d806589056cbe30946a6c7d6c9f
Mike Wu <6885201-mikewudev@users.noreply.gitlab.com>
2021-05-14 14:23:17 +0000

    Apply fixes from StyleCI

--- src/Event/template/SimpleBlog/Backgrounds/index.tsx
+++ src/Event/template/SimpleBlog/Backgrounds/index.tsx
@@ -76,7 +76,7 @@ export default function SimpleBlogBackgrounds(props: {user: Attendee}) {
               />
             </Grid>
           </HiddenOnMatch>
-))}
+        ))}
       </Grid>
     </Page>
   )
