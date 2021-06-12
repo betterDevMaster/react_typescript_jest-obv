6f5984f7b18f2bd3c90ebd7a38c337fe26b17676
Mike Wu <6885201-mikewudev@users.noreply.gitlab.com>
2021-06-12 11:05:13 +0000

    Apply fixes from StyleCI

--- src/organization/Event/Reports.tsx
+++ src/organization/Event/Reports.tsx
@@ -22,8 +22,9 @@ interface ReportsConfigContextProps {
   processing: boolean
 }
 
-const ReportsConfigContext =
-  React.createContext<undefined | ReportsConfigContextProps>(undefined)
+const ReportsConfigContext = React.createContext<
+  undefined | ReportsConfigContextProps
+>(undefined)
 
 export default function Reports() {
   const {loading, editing: report} = useReports()
