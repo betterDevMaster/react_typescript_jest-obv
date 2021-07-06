193d17dbbef831a30d6267872c33bc5692978aaf
Mike Wu <hello@mikewu.dev>
2021-07-06 21:12:39 +0900

    Hide localization import

--- src/organization/Event/LocalizationConfig/__tests__/LocalizationImport.test.tsx
+++ src/organization/Event/LocalizationConfig/__tests__/LocalizationImport.test.tsx
@@ -8,54 +8,46 @@ import {ObvioEvent} from 'Event'
 const mockPost = axios.post as jest.Mock
 
 it('should update localization values', async () => {
-  const event = fakeEvent()
-
-  const {findByLabelText, findByText} = await goToLocalizationConfig({
-    event,
-    userPermissions: [CONFIGURE_EVENTS],
-  })
-
-  const fileInput = await findByLabelText('localization import input')
-  const translationsFile = new File([], 'translations.csv')
-
-  Object.defineProperty(fileInput, 'files', {
-    value: [translationsFile],
-  })
-
-  const withImportedTranslations: ObvioEvent = {
-    ...event,
-    localization: {
-      languages: [
-        {
-          name: 'English',
-          rules: [],
-        },
-      ],
-      translations: {
-        English: {
-          foo: 'bar',
-        },
-      },
-    },
-  }
-
-  mockPost.mockImplementationOnce(() =>
-    Promise.resolve({
-      data: withImportedTranslations,
-    }),
-  )
-
-  fireEvent.change(fileInput)
-
-  await wait(() => {
-    expect(mockPost).toHaveBeenCalledTimes(1)
-  })
-
-  // Imported translations shown
-  expect(
-    ((await findByLabelText('translation key')) as HTMLInputElement).value,
-  ).toBe('foo')
-  expect(
-    ((await findByLabelText('translation value')) as HTMLInputElement).value,
-  ).toBe('bar')
+  //   const event = fakeEvent()
+  //   const {findByLabelText} = await goToLocalizationConfig({
+  //     event,
+  //     userPermissions: [CONFIGURE_EVENTS],
+  //   })
+  //   const fileInput = await findByLabelText('localization import input')
+  //   const translationsFile = new File([], 'translations.csv')
+  //   Object.defineProperty(fileInput, 'files', {
+  //     value: [translationsFile],
+  //   })
+  //   const withImportedTranslations: ObvioEvent = {
+  //     ...event,
+  //     localization: {
+  //       languages: [
+  //         {
+  //           name: 'English',
+  //           rules: [],
+  //         },
+  //       ],
+  //       translations: {
+  //         English: {
+  //           foo: 'bar',
+  //         },
+  //       },
+  //     },
+  //   }
+  //   mockPost.mockImplementationOnce(() =>
+  //     Promise.resolve({
+  //       data: withImportedTranslations,
+  //     }),
+  //   )
+  //   fireEvent.change(fileInput)
+  //   await wait(() => {
+  //     expect(mockPost).toHaveBeenCalledTimes(1)
+  //   })
+  //   // Imported translations shown
+  //   expect(
+  //     ((await findByLabelText('translation key')) as HTMLInputElement).value,
+  //   ).toBe('foo')
+  //   expect(
+  //     ((await findByLabelText('translation value')) as HTMLInputElement).value,
+  //   ).toBe('bar')
 })
