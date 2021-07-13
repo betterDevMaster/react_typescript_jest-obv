05c78f3f685176a45ed1f3b60546ef93817e7361
Mike Wu <hello@mikewu.dev>
2021-07-14 01:50:07 +0900

    Add gyls player

--- src/Event/template/SimpleBlog/Dashboard/Sidebar/index.tsx
+++ src/Event/template/SimpleBlog/Dashboard/Sidebar/index.tsx
@@ -6,10 +6,12 @@ import {ResourceList} from 'Event/template/SimpleBlog/Dashboard/ResourceList'
 import TicketRibbonList from 'Event/template/SimpleBlog/Dashboard/TicketRibbonList'
 import SidebarNav from 'Event/template/SimpleBlog/Dashboard/Sidebar/SidebarNav'
 import SidebarContainer from 'Event/template/SimpleBlog/Dashboard/Sidebar/SidebarContainer'
+import {useEvent} from 'Event/EventProvider'
 
 export default function Sidebar() {
   return (
     <SidebarContainer>
+      <GylsPlayer />
       <EmojiList />
       <TicketRibbonList />
       <AgendaList />
@@ -17,5 +19,46 @@ export default function Sidebar() {
       <ResourceList />
       <SidebarNav />
     </SidebarContainer>
+  )
+}
+
+/**
+ * TEMP - hardcode player for gyls event
+ * @returns
+ */
+function GylsPlayer() {
+  const {event} = useEvent()
+
+  const isTargetEvent = event.slug === 'gyls'
+  if (!isTargetEvent) {
+    return null
+  }
+
+  return (
+    <>
+      <p
+        style={{
+          color: '#FFFFFF',
+          fontWeight: 'bold',
+          fontSize: '15px',
+          textAlign: 'center',
+          paddingTop: '10px',
+        }}
+      >
+        JOIN AUDIO - Click the Play button below every day before you join the
+        Main Stage!(This is important so you can hear us during the breakout
+        sessions.)
+      </p>
+      <iframe
+        title="gyls player iframe"
+        src="https://mixlr.com/users/8574169/embed?autoplay=true"
+        width="100%"
+        height="150px"
+        scrolling="no"
+        frameBorder="no"
+        marginHeight={0}
+        marginWidth={0}
+      ></iframe>
+    </>
   )
 }
