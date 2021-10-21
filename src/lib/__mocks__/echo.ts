8b2297ebaee5f809fc0760b9540e8f2ac70f7d0e
Mike Wu <hello@mikewu.dev>
2021-10-21 12:18:27 +0900

    Event Socket - fix tests

--- src/lib/__mocks__/echo.ts
+++ src/lib/__mocks__/echo.ts
@@ -1,20 +1,25 @@
-export const createPrivate = jest.fn(() => ({
-  private: jest.fn(() => ({
-    listen: jest.fn(() => ({
-      pusher: {
-        connection: {
-          bind: (evt: string, cb: () => void) => {
-            /**
-             * Immediately call connected callback
-             * to mock a socket connection
-             */
-            if (evt === 'connected') {
-              cb()
-            }
-          },
+export const createPrivate = jest.fn(() => {
+  const channel: any = {
+    private: jest.fn(() => channel),
+    leave: jest.fn(),
+    error: jest.fn(() => channel),
+    listen: jest.fn(() => channel),
+    stopListening: jest.fn(),
+    pusher: {
+      connection: {
+        bind: (evt: string, cb: () => void) => {
+          /**
+           * Immediately call connected callback
+           * to mock a socket connection
+           */
+          if (evt === 'connected') {
+            cb()
+          }
         },
+        socket_id: '12345.67',
       },
-    })),
-  })),
-  leave: jest.fn(),
-}))
+    },
+  }
+
+  return channel
+})
