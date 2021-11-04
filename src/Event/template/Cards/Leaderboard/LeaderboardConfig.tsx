dc2a7e59bdc6bf76a07d65b241e8793cefbdb738
Azim Karim <azim.karim@obv.io>
2021-11-04 08:35:22 +0000

    Cards - Add snackbar config

--- src/Event/template/Cards/Leaderboard/LeaderboardConfig.tsx
+++ src/Event/template/Cards/Leaderboard/LeaderboardConfig.tsx
@@ -1,47 +1,73 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Grid from '@material-ui/core/Grid'
 import TextField from '@material-ui/core/TextField'
+import ColorPicker from 'lib/ui/ColorPicker'
 import TextEditor, {TextEditorContainer} from 'lib/ui/form/TextEditor'
 import {Controller, useForm} from 'react-hook-form'
 import {Cards, useCards} from 'Event/template/Cards'
+import {useUpdate} from 'Event/EventProvider'
+import Typography from '@material-ui/core/Typography'
+import styled from 'styled-components'
+import SnackbarContent from '@material-ui/core/SnackbarContent'
+import {onChangeStringHandler, onChangeCheckedHandler} from 'lib/dom'
+import CheckCircleIcon from '@material-ui/icons/CheckCircle'
+import Alert from '@material-ui/lab/Alert'
+import AlertTitle from '@material-ui/lab/AlertTitle'
+import Switch from 'lib/ui/form/Switch'
+import {FormControl} from '@material-ui/core'
+
 import ComponentConfig, {
   ComponentConfigProps,
   SaveButton,
 } from 'organization/Event/DashboardConfig/ComponentConfig'
-import {onChangeCheckedHandler} from 'lib/dom'
-import Switch from 'lib/ui/form/Switch'
-import {FormControl} from '@material-ui/core'
-import {useDispatchUpdate} from 'Event/TemplateProvider'
+
+export type LeaderboardConfigData = NonNullable<Cards['leaderboard']>
 
 export default function LeaderboardConfig(props: ComponentConfigProps) {
   const {template} = useCards()
-  const {leaderboard} = template
+  const {leaderboard, rewardAlert} = template
   const {register, control, handleSubmit} = useForm()
+  const [processing, setProcessing] = useState(false)
+  const updateEvent = useUpdate()
 
-  const updateTemplate = useDispatchUpdate()
+  const [rewardText, setRewardText] = useState<string>(rewardAlert.text)
+  const [rewardBackgroundColor, setRewardBackgroundColor] = useState<string>(
+    rewardAlert.backgroundColor,
+  )
+  const [rewardTextColor, setRewardTextColor] = useState<string>(
+    rewardAlert.textColor,
+  )
 
-  const submit = (data: {
-    title: string
-    description: string
-    points_unit: string
-    menuTitle: string
-    isVisible: boolean
-  }) => {
-    const existing = template.leaderboard || {}
-
+  const submit = (
+    data: LeaderboardConfigData & {points_unit: Cards['points_unit']},
+  ) => {
+    if (processing) {
+      return
+    }
     const {points_unit, ...leaderboardData} = data
 
-    const updated: Cards = {
+    setProcessing(true)
+
+    const existing = template.leaderboard || {}
+
+    const updated = {
       ...template,
+      rewardAlert: {
+        text: rewardText,
+        backgroundColor: rewardBackgroundColor,
+        textColor: rewardTextColor,
+      },
+      points_unit,
       leaderboard: {
         ...existing,
         ...leaderboardData,
       },
-      points_unit,
     }
 
-    updateTemplate(updated)
-
-    props.onClose()
+    updateEvent({template: updated}).finally(() => {
+      setProcessing(false)
+      props.onClose()
+    })
   }
 
   return (
@@ -51,68 +77,185 @@ export default function LeaderboardConfig(props: ComponentConfigProps) {
       title="Leaderboard"
     >
       <form onSubmit={handleSubmit(submit)}>
-        <FormControl>
-          <Controller
-            name="isVisible"
-            control={control}
-            defaultValue={leaderboard.isVisible}
-            render={({value, onChange}) => (
-              <Switch
-                checked={value}
-                onChange={onChangeCheckedHandler(onChange)}
-                arial-label="toggle points"
-                labelPlacement="end"
-                color="primary"
-                label="Enabled"
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <FormControl>
+              <Controller
+                name="isVisible"
+                control={control}
+                defaultValue={leaderboard.isVisible}
+                render={({value, onChange}) => (
+                  <Switch
+                    checked={value}
+                    onChange={onChangeCheckedHandler(onChange)}
+                    arial-label="toggle points"
+                    labelPlacement="end"
+                    color="primary"
+                    label="Enabled"
+                  />
+                )}
               />
-            )}
-          />
-        </FormControl>
-        <TextField
-          defaultValue={leaderboard.title}
-          name="title"
-          label="Title"
-          fullWidth
-          inputProps={{
-            'aria-label': 'set leaderboard page title',
-            ref: register({required: 'Title is required'}),
-          }}
-        />
-        <Controller
-          name="description"
-          control={control}
-          defaultValue={leaderboard.description}
-          rules={{
-            required: 'Description is required',
-          }}
-          render={({value, onChange}) => (
-            <TextEditorContainer>
-              <TextEditor data={value} onChange={onChange} />
-            </TextEditorContainer>
-          )}
-        />
-        <TextField
-          defaultValue={leaderboard.menuTitle}
-          name="menuTitle"
-          label="Menu Title"
-          fullWidth
-          inputProps={{
-            'aria-label': 'set leaderboard page menu title',
-            ref: register({required: 'Title is required'}),
-          }}
-        />
-        <TextField
-          defaultValue={template.points_unit}
-          name="points_unit"
-          label="Points Unit"
-          fullWidth
-          inputProps={{
-            ref: register({required: 'Points unit is required'}),
-            'aria-label': 'points unit',
-          }}
-        />
+            </FormControl>
+          </Grid>
+          <Grid item xs={12}>
+            <Alert severity="info">
+              <AlertTitle>Variables</AlertTitle>
+              <div>
+                <Typography variant="caption">
+                  {`{{points_unit}} - Name for points`}
+                </Typography>
+              </div>
+              <div>
+                <Typography variant="caption">
+                  {`{{action_description}} - Action description`}
+                </Typography>
+              </div>
+              <div>
+                <Typography variant="caption">
+                  {`{{action_points}} - Number of points received`}
+                </Typography>
+              </div>
+              <div>
+                <Typography variant="caption">
+                  {`{{leaderboard_points}} - Attendee's current points`}
+                </Typography>
+              </div>
+              <div>
+                <Typography variant="caption">
+                  {`{{leaderboard_position}} - Attendee's current position on leaderboard`}
+                </Typography>
+              </div>
+            </Alert>
+          </Grid>
+
+          <Grid item xs={12}>
+            <TextField
+              name="points_unit"
+              defaultValue={template.points_unit}
+              label="Unit"
+              fullWidth
+              inputProps={{
+                'aria-label': 'points unit',
+                ref: register,
+              }}
+              disabled={processing}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              defaultValue={leaderboard.title}
+              name="title"
+              label="Title"
+              fullWidth
+              inputProps={{
+                'aria-label': 'set leaderboard page title',
+                ref: register({required: 'Title is required'}),
+              }}
+              disabled={processing}
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            <TextField
+              defaultValue={leaderboard.menuTitle}
+              name="menuTitle"
+              label="Menu Title"
+              fullWidth
+              inputProps={{
+                'aria-label': 'set leaderboard menu title',
+                ref: register,
+              }}
+              disabled={processing}
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            <Controller
+              name="description"
+              control={control}
+              defaultValue={leaderboard.description}
+              rules={{
+                required: 'Description is required',
+              }}
+              render={({value, onChange}) => (
+                <TextEditorContainer>
+                  <TextEditor
+                    data={value}
+                    onChange={onChange}
+                    disabled={processing}
+                  />
+                </TextEditorContainer>
+              )}
+            />
+          </Grid>
+          <Grid item xs={12} md={12}>
+            <StyledSnackerBar
+              message={
+                <Continer>
+                  <CheckCircleIcon />
+                  <StyledDiv>{rewardText}</StyledDiv>
+                </Continer>
+              }
+              color={rewardTextColor}
+              backgroundColor={rewardBackgroundColor}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <ColorPicker
+              label="Popup Background Color"
+              color={rewardBackgroundColor}
+              onPick={setRewardBackgroundColor}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <ColorPicker
+              label="Popup Color"
+              color={rewardTextColor}
+              onPick={setRewardTextColor}
+            />
+          </Grid>
+          <Grid item xs={12} md={12}>
+            <TextField
+              defaultValue={rewardText}
+              name="popupText"
+              label="Popup Text"
+              fullWidth
+              multiline
+              rows={3}
+              variant="outlined"
+              onChange={onChangeStringHandler(setRewardText)}
+              inputProps={{
+                'aria-label': 'set leaderboard popup text',
+                ref: register({required: 'Title is required'}),
+              }}
+              disabled={processing}
+            />
+          </Grid>
+        </Grid>
         <SaveButton>Save</SaveButton>
       </form>
     </ComponentConfig>
   )
 }
+
+const StyledSnackerBar = styled(
+  ({
+    color,
+    backgroundColor,
+    ...otherProps
+  }: {
+    color: string
+    backgroundColor: string
+    message: React.ReactElement
+  }) => <SnackbarContent {...otherProps} />,
+)`
+  color: ${(props) => props.color}!important;
+  background-color: ${(props) => props.backgroundColor} !important;
+`
+
+const Continer = styled.div`
+  display: flex;
+`
+const StyledDiv = styled.div`
+  font-size: 19px;
+`
