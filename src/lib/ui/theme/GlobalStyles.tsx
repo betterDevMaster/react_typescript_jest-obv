96b7708ccdf6f7f7147b37d72ce0f2289c22b86b
Darwin <darwin@obv.io>
2021-12-22 16:00:13 -0500

    [Add] global thumb style

--- src/lib/ui/theme/GlobalStyles.tsx
+++ src/lib/ui/theme/GlobalStyles.tsx
@@ -17,4 +17,48 @@ export const GlobalStyles = createGlobalStyle`
   a {
     text-decoration: none;
   }
+
+  *::-webkit-scrollbar {
+    width: 7px;
+    height: 7px;
+  }
+
+  *::-webkit-scrollbar-button {
+      width: 0px;
+      height: 0px;
+  }
+
+  *::-webkit-scrollbar-thumb {
+      background: #303c54db;
+      border: 1px solid #2b3654;
+      border-radius: 5px;
+      transition: .5s;
+  }
+
+  *::-webkit-scrollbar-thumb:hover {
+      background: #142449;
+      cursor: pointer;
+  }
+
+  *::-webkit-scrollbar-thumb:active {
+      background: #142449;
+  }
+
+  *::-webkit-scrollbar-track {
+      background: #ffffff;
+      border-left: 1px solid #c8c9cd8f;
+      border-radius: 0;
+  }
+
+  *::-webkit-scrollbar-track:hover {
+      background: #ffffff;
+  }
+
+  *::-webkit-scrollbar-track:active {
+      background: #ffffff;
+  }
+
+  *::-webkit-scrollbar-corner {
+      background: transparent;
+  }
 `
